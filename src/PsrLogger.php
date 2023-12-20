c53dc905b2a3ac2685690ec5faf5fba72ddeebc1
Amine Jaaouani <amine@supermood.fr>
2023-12-20 16:43:56 +0100

    feat: google/cloud-logging is now compatible with psr/log >=v3.0

--- src/PsrLogger.php
+++ src/PsrLogger.php
@@ -168,7 +168,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function emergency($message, array $context = [])
+    public function emergency($message, array $context = []): void
     {
         $this->log(Logger::EMERGENCY, $message, $context);
     }
@@ -186,7 +186,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function alert($message, array $context = [])
+    public function alert($message, array $context = []): void
     {
         $this->log(Logger::ALERT, $message, $context);
     }
@@ -204,7 +204,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function critical($message, array $context = [])
+    public function critical($message, array $context = []): void
     {
         $this->log(Logger::CRITICAL, $message, $context);
     }
@@ -222,7 +222,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function error($message, array $context = [])
+    public function error($message, array $context = []): void
     {
         $this->log(Logger::ERROR, $message, $context);
     }
@@ -240,7 +240,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function warning($message, array $context = [])
+    public function warning($message, array $context = []): void
     {
         $this->log(Logger::WARNING, $message, $context);
     }
@@ -258,7 +258,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function notice($message, array $context = [])
+    public function notice($message, array $context = []): void
     {
         $this->log(Logger::NOTICE, $message, $context);
     }
@@ -276,7 +276,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function info($message, array $context = [])
+    public function info($message, array $context = []): void
     {
         $this->log(Logger::INFO, $message, $context);
     }
@@ -294,7 +294,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      *        for the available options.
      * @return void
      */
-    public function debug($message, array $context = [])
+    public function debug($message, array $context = []): void
     {
         $this->log(Logger::DEBUG, $message, $context);
     }
@@ -371,7 +371,7 @@ class PsrLogger implements LoggerInterface, \Serializable
      * @return void
      * @throws \InvalidArgumentException
      */
-    public function log($level, $message, array $context = [])
+    public function log($level, $message, array $context = []): void
     {
         $this->validateLogLevel($level);
         $options = [];
